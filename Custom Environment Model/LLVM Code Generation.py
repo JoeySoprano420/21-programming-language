0e72f708add14b058dfdbe6b34212a1d0a6344df
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-10-16 21:09:58 -0400

    Update LLVM Code Generation.py

--- Custom Environment Model/LLVM Code Generation.py
+++ Custom Environment Model/LLVM Code Generation.py
@@ -1,6 +1,3 @@
-import llvmlite.ir as ir
-import llvmlite.binding as llvm
-
 class LLVMCodeGenerator:
     def __init__(self):
         # Initialize LLVM
@@ -11,13 +8,15 @@ class LLVMCodeGenerator:
         # Create the module and function builder
         self.module = ir.Module(name='Dynamic21')
         self.builder = None
+        self.variables = {}  # To store variable references
 
         # Setup function signatures
         self.int_type = ir.IntType(32)
         self.bool_type = ir.IntType(1)
 
     def create_function(self):
-        func_type = ir.FunctionType(self.int_type, [self.int_type])
+        # Define a 'main' function
+        func_type = ir.FunctionType(self.int_type, [])
         function = ir.Function(self.module, func_type, name="main")
         block = function.append_basic_block(name="entry")
         self.builder = ir.IRBuilder(block)
@@ -26,12 +25,24 @@ class LLVMCodeGenerator:
     def create_var(self, name, value):
         var = self.builder.alloca(self.int_type, name=name)
         self.builder.store(ir.Constant(self.int_type, value), var)
+        self.variables[name] = var
         return var
 
-    def add_instruction(self, reg, val):
-        loaded_reg = self.builder.load(reg, name='load_reg')
-        added = self.builder.add(loaded_reg, ir.Constant(self.int_type, val))
-        self.builder.store(added, reg)
+    def assign_var(self, register, value):
+        if register in self.variables:
+            var = self.variables[register]
+            self.builder.store(ir.Constant(self.int_type, value), var)
+        else:
+            raise ValueError(f"Variable {register} not found!")
+
+    def add_instruction(self, reg, identifier):
+        if reg in self.variables and identifier in self.variables:
+            reg_val = self.builder.load(self.variables[reg])
+            id_val = self.builder.load(self.variables[identifier])
+            added_val = self.builder.add(reg_val, id_val)
+            self.builder.store(added_val, self.variables[reg])
+        else:
+            raise ValueError(f"Variables {reg} or {identifier} not found!")
 
     def generate_code(self):
         print(str(self.module))
@@ -49,10 +60,26 @@ class LLVMCodeGenerator:
         with open('output.o', 'wb') as f:
             f.write(target_machine.emit_object(mod))
 
-# Usage Example
-codegen = LLVMCodeGenerator()
-main_func = codegen.create_function()
-x = codegen.create_var('x', 10)
-codegen.add_instruction(x, 5)
-codegen.generate_code()
-codegen.execute_code()
+    # Traverse the AST and generate LLVM code
+    def generate_from_ast(self, ast):
+        function = self.create_function()
+
+        for node in ast:
+            if node[0] == 'var_decl':
+                # Variable declaration
+                _, var_type, var_name, var_value = node
+                self.create_var(var_name, var_value)
+
+            elif node[0] == 'assign':
+                # Assignment operation
+                _, reg, value = node
+                self.assign_var(reg, value)
+
+            elif node[0] == 'add':
+                # Addition operation
+                _, reg, identifier = node
+                self.add_instruction(reg, identifier)
+
+        # Close function by returning 0
+        self.builder.ret(ir.Constant(self.int_type, 0))
+
