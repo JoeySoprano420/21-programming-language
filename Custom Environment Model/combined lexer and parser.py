204daf60505aa9bb559f70f5be3296d5667f3272
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-10-16 21:24:11 -0400

    Update combined lexer and parser.py

--- Custom Environment Model/combined lexer and parser.py
+++ Custom Environment Model/combined lexer and parser.py
@@ -3,21 +3,28 @@ import ply.yacc as yacc
 import llvmlite.ir as ir
 import llvmlite.binding as llvm
 
+# Tokens
 tokens = [
-    'VAR', 'REGISTER', 'INT', 'BOOL', 'IF', 'EQUAL', 'NUMBER', 'IDENTIFIER', 'ARROW', 'ADD', 'IS', 'OUTPUT'
+    'VAR', 'REGISTER', 'INT', 'BOOL', 'IF', 'ELIF', 'EQUAL', 'NUMBER', 'IDENTIFIER', 'ARROW', 'ADD', 'IS', 'OUTPUT', 'GT', 'LT', 'GE', 'LE'
 ]
 
 # Token definitions
 t_VAR = r'var'
-t_REGISTER = r'eax|ebx|ecx'
+t_REGISTER = r'eax|ebx|ecx|edx'
 t_IF = r'<IF>'
+t_ELIF = r'<ELIF>'
 t_EQUAL = r'='
 t_ARROW = r'<-'
 t_ADD = r'add'
 t_IS = r':IS:'
 t_OUTPUT = r'@|'
+t_GT = r'>'
+t_LT = r'<'
+t_GE = r'>='
+t_LE = r'<='
 t_ignore = ' \t'
 
+# Identifiers and numbers
 def t_IDENTIFIER(t):
     r'[a-zA-Z_]\w*'
     return t
@@ -27,13 +34,15 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
+# Error handling
 def t_error(t):
     print(f"Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
 
+# Build the lexer
 lexer = lex.lex()
 
-# PARSER
+# Parser rules
 
 def p_statement_var_decl(p):
     '''statement : VAR ':' type '[' IDENTIFIER EQUAL NUMBER ']' '''
@@ -52,18 +61,22 @@ def p_if_condition(p):
     p[0] = ('if', p[2], p[4])
 
 def p_condition(p):
-    '''condition : IDENTIFIER ':' IS condition_check'''
+    '''condition : IDENTIFIER IS condition_check'''
     p[0] = ('condition', p[1], p[3])
 
 def p_condition_check(p):
-    '''condition_check : '[' NUMBER '>' NUMBER ']' '''
-    p[0] = ('range', p[2], p[4])
+    '''condition_check : NUMBER GT NUMBER
+                       | NUMBER LT NUMBER
+                       | NUMBER GE NUMBER
+                       | NUMBER LE NUMBER '''
+    p[0] = ('comparison', p[2], p[1], p[3])
 
-def p_action(p):
-    '''action : '@|' OUTPUT IDENTIFIER '|@' '''
-    p[0] = ('output', p[3])
+def p_action_output(p):
+    '''action : OUTPUT IDENTIFIER'''
+    p[0] = ('output', p[2])
 
 def p_error(p):
     print("Syntax error in input!")
 
+# Build the parser
 parser = yacc.yacc()
